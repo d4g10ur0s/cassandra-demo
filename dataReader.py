bf01c92b067930ae47924ebfdbeead20327f8125
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2023-07-01 21:41:51 +0300

    Add files via upload

--- dataReader.py
+++ dataReader.py
@@ -1,17 +1,21 @@
 import os
 
 import pandas as pd
+import numpy as np
+import ast
 
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 from cassandra.cluster import Cluster
 from cassandra.cqlengine.management import sync_table
+from cassandra.query import BatchStatement, SimpleStatement
+from cassandra import ConsistencyLevel
 
 def createRecipeTable(session):
     session.execute("""CREATE TABLE recipe (
                        id bigint PRIMARY KEY,
                        contributor_id bigint,
-                       minutes int,
+                       minutes float,
                        mean_rating double,
                        name text,
                        submitted date,
@@ -24,6 +28,39 @@ def createRecipeTable(session):
                        n_ingredients int
                        )""")
 
+def recipeBulkInsert(recipes,session):
+    #recipes.fillna(0,inplace=True)
+    tableOrder = ["id","contributor_id","minutes","mean rating","name","submitted",
+                  "tags","nutrition","n_steps","steps","description","ingredients","n_ingredients"]
+    insertStatement = "insert into recipe (id,contributor_id,minutes,mean_rating,name,submitted,"+"tags,nutrition,n_steps,steps,description,ingredients,n_ingredients) VALUES ("+"?,"*12+"?"+")"
+    insertRecipes = session.prepare(insertStatement)
+    batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
+    counter = 0
+    for recipe in recipes[tableOrder].values.tolist():
+        counter+=1
+        try :
+            if np.isnan(recipe[4]):
+                recipe[4]="noName"
+        except:
+            pass
+        recipe[6] = ast.literal_eval(recipe[6])
+        recipe[7] = ast.literal_eval(recipe[7])
+        recipe[9] = ast.literal_eval(recipe[9])
+        try :
+            if np.isnan(recipe[10]):
+                recipe[10]=""
+        except:
+            pass
+        recipe[11] = ast.literal_eval(recipe[11])
+        print(str(recipe[10]))
+        batch.add(insertRecipes, tuple(recipe))
+        if counter > 10:
+            session.execute(batch)
+            counter = 0
+            batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
+        else:
+            pass
+    session.execute(batch)
 # create csv data if it doesnt exist
 if os.path.exists(os.getcwd()+"/processed_recipes.csv"):
     pass
@@ -48,21 +85,18 @@ cluster = Cluster(['127.0.0.1'])
 session = cluster.connect()
 # use the correct keyspace
 session.execute("use recipesharing;")
-##
-## Test
-row = session.execute("SELECT release_version FROM system.local")
-if row:
-    print(row[0])
-##Test
-##
 # try get table data
 try :
-    rows = session.execute("SELECT * FROM test_table WHERE id=%s", [2])
+    rows = session.execute("SELECT * FROM recipe", [])
     if not rows:
         print("Does not exist")
 except:
     createRecipeTable(session)
 
+recipeBulkInsert(editedRecipes,session)
+rows = session.execute("SELECT * FROM recipe", [])
+if not rows:
+    print("Does not exist")
 '''
 ingridientMapping = pd.read_pickle("ingr_map.pkl")
 print(str(ingridientMapping.keys()))
