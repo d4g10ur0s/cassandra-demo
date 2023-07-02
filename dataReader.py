d2b7d28825f3303849f226a9cb1bfd2bf022825a
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2023-07-02 20:31:22 +0300

    Add files via upload

--- dataReader.py
+++ dataReader.py
@@ -12,27 +12,30 @@ from cassandra.query import BatchStatement, SimpleStatement
 from cassandra import ConsistencyLevel
 
 def createRecipeTable(session):
-    session.execute("""CREATE TABLE recipe (
-                       id bigint PRIMARY KEY,
+    session.execute("""CREATE TABLE IF NOT EXISTS recipe (
+                       id bigint ,
                        contributor_id bigint,
                        minutes float,
                        mean_rating double,
                        name text,
                        submitted date,
-                       tags set<text>,
                        nutrition set<double>,
                        n_steps int,
                        steps set<text>,
                        description text,
                        ingredients set<text>,
-                       n_ingredients int
-                       )""")
+                       n_ingredients int,
+                       PRIMARY KEY (difficulty) );""")
+    session.execute("""CREATE TABLE IF NOT EXISTS recipe_tags (
+                       id bigint ,
+                       tag_name text,
+                       PRIMARY KEY(tag_name,id) );""")
 
 def recipeBulkInsert(recipes,session):
     #recipes.fillna(0,inplace=True)
     tableOrder = ["id","contributor_id","minutes","mean rating","name","submitted",
-                  "tags","nutrition","n_steps","steps","description","ingredients","n_ingredients"]
-    insertStatement = "insert into recipe (id,contributor_id,minutes,mean_rating,name,submitted,"+"tags,nutrition,n_steps,steps,description,ingredients,n_ingredients) VALUES ("+"?,"*12+"?"+")"
+                  "nutrition","n_steps","steps","description","ingredients","n_ingredients"]
+    insertStatement = "insert into recipe (id,contributor_id,minutes,mean_rating,name,submitted,"+"nutrition,n_steps,steps,description,ingredients,n_ingredients) VALUES ("+"?,"*11+"?"+")"
     insertRecipes = session.prepare(insertStatement)
     batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
     counter = 0
@@ -44,15 +47,15 @@ def recipeBulkInsert(recipes,session):
         except:
             pass
         recipe[6] = ast.literal_eval(recipe[6])
-        recipe[7] = ast.literal_eval(recipe[7])
-        recipe[9] = ast.literal_eval(recipe[9])
+        #recipe[7] = ast.literal_eval(recipe[7])
+        recipe[8] = ast.literal_eval(recipe[8])
         try :
-            if np.isnan(recipe[10]):
-                recipe[10]=""
+            if np.isnan(recipe[9]):
+                recipe[9]=""
         except:
             pass
-        recipe[11] = ast.literal_eval(recipe[11])
-        print(str(recipe[10]))
+        recipe[10] = ast.literal_eval(recipe[10])
+        print(str(recipe[9]))
         batch.add(insertRecipes, tuple(recipe))
         if counter > 10:
             session.execute(batch)
@@ -66,7 +69,18 @@ if os.path.exists(os.getcwd()+"/processed_recipes.csv"):
     pass
 else:
     print("Creating CSV")
+    #create difficulty
     recipesRaw = pd.read_csv("RAW_recipes.csv")
+    recipesRaw[["minutes","n_steps"]]=recipesRaw[["minutes","n_steps"]].fillna(0,inplace=False)
+    maxDifficulty = recipesRaw["minutes"].max() * recipesRaw["n_steps"].max()
+    recipesRaw["difficulty"] = recipesRaw["minutes"]*recipesRaw["n_steps"]/maxDifficulty
+    dist = recipesRaw["difficulty"].max()-recipesRaw["difficulty"].min()
+    diffRange = [i/4 * dist for i in range(1,4) ] + [recipesRaw["difficulty"].max(),]
+    zeroSkill = recipesRaw[recipesRaw["difficulty"]<=diffRange[0]]
+    easy = recipesRaw[recipesRaw["difficulty"]<=diffRange[1]]
+    intermediate = recipesRaw[recipesRaw["difficulty"]<=diffRange[2]]
+    professional = recipesRaw[recipesRaw["difficulty"]>diffRange[2]]
+    #############################################################################
     interactionsRaw = pd.read_csv("RAW_interactions.csv")
     recipeId = recipesRaw["id"].values.tolist()
     # get mean rating
@@ -89,14 +103,18 @@ session.execute("use recipesharing;")
 try :
     rows = session.execute("SELECT * FROM recipe", [])
     if not rows:
-        print("Does not exist")
+        print("Data doesn\'t exist")
+        print("Inserting data")
+        recipeBulkInsert(editedRecipes,session)
 except:
     createRecipeTable(session)
 
-recipeBulkInsert(editedRecipes,session)
+# SELECT * FROM recipe WHERE submitted >= '2012-01-01' AND  submitted <= '2012-05-31' ORDER BY mean_value ASC LIMIT 100 ALLOW FILTERING;
+
 rows = session.execute("SELECT * FROM recipe", [])
 if not rows:
     print("Does not exist")
+
 '''
 ingridientMapping = pd.read_pickle("ingr_map.pkl")
 print(str(ingridientMapping.keys()))
