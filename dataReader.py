3475a9b5de737a961deaba1b7b3bd6150358402f
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2023-07-03 02:13:16 +0300

    Add files via upload

--- dataReader.py
+++ dataReader.py
@@ -11,66 +11,7 @@ from cassandra.cqlengine.management import sync_table
 from cassandra.query import BatchStatement, SimpleStatement
 from cassandra import ConsistencyLevel
 
-def createRecipeTable(session):
-    session.execute("""CREATE TABLE IF NOT EXISTS recipe (
-                       id bigint ,
-                       contributor_id bigint,
-                       minutes float,
-                       mean_rating double,
-                       name text,
-                       submitted date,
-                       nutrition set<double>,
-                       n_steps int,
-                       steps set<text>,
-                       description text,
-                       ingredients set<text>,
-                       n_ingredients int,
-                       difficulty text,
-                       PRIMARY KEY ((difficulty) , mean_rating , n_steps) )
-                       WITH CLUSTERING ORDER BY (mean_rating ASC);""")
-    session.execute("""CREATE TABLE IF NOT EXISTS recipe_tags (
-                       id bigint ,
-                       tag_name text,
-                       PRIMARY KEY((tag_name,id)) );""")
-# select id from recipe_tags where tag_name="auto pu 8eloume" ;
-# select * from recipe where id in [] ;
-def recipeBulkInsert(recipes,session):
-    #recipes.fillna(0,inplace=True)
-    tableOrder = ["id","contributor_id","minutes","mean rating","name","submitted",
-                  "nutrition","n_steps","steps","description","ingredients","n_ingredients","difficulty"]
-    insertStatement = "insert into recipe (id,contributor_id,minutes,mean_rating,name,submitted,"+"nutrition,n_steps,steps,description,ingredients,n_ingredients,difficulty) VALUES ("+"?,"*12+"?"+")"
-    insertRecipes = session.prepare(insertStatement)
-    batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
-    counter = 0
-    for recipe in recipes[tableOrder].values.tolist():
-        counter+=1
-        try :
-            if np.isnan(recipe[4]):
-                recipe[4]="noName"
-        except:
-            pass
-        recipe[6] = ast.literal_eval(recipe[6])
-        #recipe[7] = ast.literal_eval(recipe[7])
-        recipe[8] = ast.literal_eval(recipe[8])
-        try :
-            if np.isnan(recipe[9]):
-                recipe[9]=""
-        except:
-            pass
-        recipe[10] = ast.literal_eval(recipe[10])
-        print(str(recipe[9]))
-        batch.add(insertRecipes, tuple(recipe))
-        if counter > 10:
-            session.execute(batch)
-            counter = 0
-            batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
-        else:
-            pass
-    session.execute(batch)
-# create csv data if it doesnt exist
-if os.path.exists(os.getcwd()+"/processed_recipes.csv"):
-    pass
-else:
+def createCSV():
     print("Creating CSV")
     # create difficulty
     # Objective : 3
@@ -102,26 +43,127 @@ else:
     # add mean rating to dataframe
     recipesRaw["mean rating"] = recipeMeanRating
     recipesRaw.to_csv("processed_recipes.csv")
-# read csv data
-print("Reading CSV")
-editedRecipes = pd.read_csv("processed_recipes.csv")
+
+def query_1(session):
+    typeOfRecipe=["zeroSkill","easy","intermediate","professional"]
+    chosen = []
+    for i in typeOfRecipe:
+        statement = "SELECT name,mean_rating FROM recipe WHERE difficulty=\'"+ str(i)+"\' and submitted >= '2012-01-01' AND  submitted <= '2012-05-31' ORDER BY mean_rating DESC LIMIT 30 ALLOW FILTERING;"
+        result = session.execute(statement)
+        print(str(i))
+        for res in result:
+            print(str(res))
+
+def query_2(session):
+    statement = "SELECT name,mean_rating FROM recipe WHERE name=\'"+ str(input("Give name of recipe : "))+"\' ALLOW FILTERING;"
+    result = session.execute(statement)
+    print(str(i))
+    for res in result:
+        print(str(res))
+
+def query_3(session):
+    typeOfRecipe=["zeroSkill","easy","intermediate","professional"]
+    print("Choose number of difficulty : \n Select between 1 to 4")
+    for i in typeOfRecipe:
+        print(str(i))
+    choice = int(input())
+    print("Selected : " + str(typeOfRecipe[choice]))
+    statement = "SELECT name,mean_rating FROM recipe WHERE difficulty=\'"+ str(typeOfRecipe[choice])+"\' ORDER BY mean_rating DESC LIMIT 100 ALLOW FILTERING;"
+    result = session.execute(statement)
+    for res in result:
+        print(str(res))
+
+def createRecipeTable(session):
+    session.execute("""CREATE TABLE IF NOT EXISTS recipe (
+                       id bigint ,
+                       contributor_id bigint,
+                       minutes float,
+                       mean_rating double,
+                       name text,
+                       submitted date,
+                       nutrition set<double>,
+                       n_steps int,
+                       steps set<text>,
+                       description text,
+                       ingredients set<text>,
+                       n_ingredients int,
+                       difficulty text,
+                       PRIMARY KEY ((difficulty) , mean_rating , n_steps) )
+                       WITH CLUSTERING ORDER BY (mean_rating DESC);""")
+    session.execute("""CREATE TABLE IF NOT EXISTS recipe_tags (
+                       id bigint ,
+                       tag_name text,
+                       PRIMARY KEY((tag_name,id)) );""")
+# select id from recipe_tags where tag_name="auto pu 8eloume" ;
+# select * from recipe where id in [] ;
+def recipeBulkInsert(recipes,session):
+    tableOrder = ["id","contributor_id","minutes","mean rating","name","submitted",
+                  "nutrition","n_steps","steps","description","ingredients","n_ingredients","difficulty"]
+    insertStatement = "insert into recipe (id,contributor_id,minutes,mean_rating,name,submitted,"+"nutrition,n_steps,steps,description,ingredients,n_ingredients,difficulty) VALUES ("+"?,"*12+"?"+")"
+    insertRecipes = session.prepare(insertStatement)
+    batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
+    counter = 0
+    acounter = 0
+    for recipe in recipes[tableOrder].values.tolist():
+        counter+=1
+        acounter+=1
+        print(str(acounter))
+        try :
+            if np.isnan(recipe[4]):
+                recipe[4]="noName"
+        except:
+            pass
+        recipe[6] = ast.literal_eval(recipe[6])
+        #recipe[7] = ast.literal_eval(recipe[7])
+        recipe[8] = ast.literal_eval(recipe[8])
+        try :
+            if np.isnan(recipe[9]):
+                recipe[9]=""
+        except:
+            pass
+        recipe[10] = ast.literal_eval(recipe[10])
+        batch.add(insertRecipes, tuple(recipe))
+        if counter > 10:
+            session.execute(batch)
+            counter = 0
+            batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
+        else:
+            pass
+    session.execute(batch)
+# create csv data if it doesnt exist
+if os.path.exists(os.getcwd()+"/processed_recipes.csv"):
+    pass
+else:
+    createCSV()
 # connect to cassandra
 print("Connecting to database")
 cluster = Cluster(['127.0.0.1'])
 session = cluster.connect()
-# use the correct keyspace
-session.execute("use recipesharing;")
-# try get table data
-try :
+session.execute("use recipesharing;")# use the correct keyspace
+
+try :# try get table data
     rows = session.execute("SELECT * FROM recipe", [])
     if not rows:
         print("Data doesn\'t exist")
         print("Inserting data")
+        # read csv data
+        print("Reading CSV")
+        editedRecipes = pd.read_csv("processed_recipes.csv")
+        recipeBulkInsert(editedRecipes,session)
+    while 1 :
+        try :
+            choice = int(input("Choose an integer between 1 to 5\n"))
+            if choice==1:
+                recipes = query_1(session)
+            elif choice==2:
+                recipes = query_2(session)
+            elif choice==3:
+                recipes = query_3(session)
+        except:
+            if (input("Do you want to exit?\n(y\\n)\n")) ==="y":
+                break
 except:
     createRecipeTable(session)
-
-# SELECT name FROM recipe WHERE difficulty='easy' and submitted >= '2012-01-01' AND  submitted <= '2012-05-31' ORDER BY mean_rating ASC LIMIT 100 ALLOW FILTERING;
-recipeBulkInsert(editedRecipes,session)
 
 rows = session.execute("SELECT * FROM recipe", [])
 if not rows:
